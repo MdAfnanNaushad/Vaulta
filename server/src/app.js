6408fb8553ac68a62821c898f141fbc9db190d11
MdAfnanNaushad <mdafnannaushad@gmail.com>
2026-06-11 15:02:04 +0530

    Frontend render config

--- server/src/app.js
+++ server/src/app.js
@@ -3,19 +3,23 @@ import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
+
 import errorMiddleware from "./middleware/error.middleware.js";
 import v1Routes from "./api/v1/routes/index.js";
+
 import {
   swaggerSpec,
   swaggerUi,
 } from "./docs/swagger.js";
 
-
 const app = express();
 
 app.use(
   cors({
-    origin: true,
+    origin: [
+      "http://localhost:5173",
+      "https://vaulta-frontend-5xoo.onrender.com",
+    ],
     credentials: true,
   })
 );
@@ -24,28 +28,13 @@ app.use(helmet());
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(cookieParser());
-app.use(errorMiddleware);
-
-app.use(
-  "/api/v1",
-  v1Routes
-);
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(
-    swaggerSpec
-  )
-);
-
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec)
-);
 
 app.use(
   rateLimit({
@@ -53,5 +42,15 @@ app.use(
     max: 100,
   })
 );
+
+app.use("/api/v1", v1Routes);
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec)
+);
+
+app.use(errorMiddleware);
 
 export default app;
